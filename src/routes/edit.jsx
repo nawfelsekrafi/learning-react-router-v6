cb7f0b6c9d23debabd9cca2ed924e0b22213bb52
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2022-12-20 14:18:52 +0100

    added cancel btn handler using useNavigate and navigate -1

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,6 +1,5 @@
-import { Form, useLoaderData, redirect } from "react-router-dom";
+import { Form, useLoaderData, redirect, useNavigate } from "react-router-dom";
 import { updateContact } from "../contacts";
-
 
 export async function action({ request, params }) {
   const formData = await request.formData();
@@ -11,6 +10,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
   const contact = useLoaderData();
+  const navigate = useNavigate();
 
   return (
     <Form method="post" id="contact-form">
@@ -56,7 +56,14 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button
+          type="button"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          Cancel
+        </button>
       </p>
     </Form>
   );
